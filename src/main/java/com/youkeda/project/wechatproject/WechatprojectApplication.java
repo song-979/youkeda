02fc48ed7cffd2270b6592cf6555acc7e6fa4cfd
song-979 <2870991815@qq.com>
2026-07-15 12:37:14 +0800

    Initialize Spring Boot project skeleton
    Spring Boot 2.6.13 + Java project with web and MySQL dependencies.

---
+++ src/main/java/com/youkeda/project/wechatproject/WechatprojectApplication.java
@@ -0,0 +1,13 @@
+package com.youkeda.project.wechatproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youkeda/project/wechatproject/WechatprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youkeda.project.wechatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
