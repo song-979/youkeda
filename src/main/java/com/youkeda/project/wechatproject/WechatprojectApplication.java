c25479c080c08525be3e8e6b8b5fd42f064474ba
fei123-marker <q320923@qq.com>
2026-07-16 11:45:22 +0800

    Merge branch 'main' of https://github.com/song-979/youkeda

--- src/main/java/com/youkeda/project/wechatproject/WechatprojectApplication.java
+++ src/main/java/com/youkeda/project/wechatproject/WechatprojectApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WechatprojectApplication {
-
     public static void main(String[] args) {
+        System.out.println("调试");
         SpringApplication.run(WechatprojectApplication.class, args);
     }
 
